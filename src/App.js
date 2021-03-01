26161fe1e1e52da5daaf732ba789f0b1c37ac4ce
Sol1udeee <sergey.lishtvan01@gmail.com>
2021-03-01 23:46:35 +0200

    finish

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
+import { Subject, interval } from "rxjs";
+import { takeUntil } from 'rxjs/operators';
+import { useEffect, useState } from 'react';
 
-function App() {
+const App = () => {
+  const [seconds, setSeconds] = useState(0);
+  const [status, setStatus] = useState('stop');
+  const [clicked, setClicked] = useState(false)
+
+  useEffect(() => {
+    const unsubscribe$ = new Subject();
+    interval(1000)
+      .pipe(takeUntil(unsubscribe$))
+      .subscribe(() => {
+        if (status === 'run') {
+          setSeconds(seconds => seconds + 1000);
+        }
+      });
+
+    return () => {
+      unsubscribe$.next();
+      unsubscribe$.complete();
+    };
+  }, [status]);
+
+  const stopBtn = () => {
+    setStatus('stop');
+    setSeconds(0);
+  }
+
+  const waitBtn = () => {
+    if (clicked) setStatus('wait');
+    setClicked(true)
+    setTimeout(() => {
+      setClicked(false)
+    }, 200);
+  }
+
+  const startBtn = () => setStatus('run');
+  const resetBtn = () => setSeconds(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <p>{new Date(seconds).toISOString().slice(11, 19)}</p>
+      <div>
+        <button onClick={startBtn}>Start</button>
+        <button onClick={stopBtn}>Stop</button>
+        <button onClick={resetBtn}>Reset</button>
+        <button onClick={waitBtn}>Wait</button>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
